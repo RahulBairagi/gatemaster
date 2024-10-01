e4549f73e1a523b83cd893ea11faf1bc1d7de7de
cca <audhomneni@ccamatil.com>
2024-10-01 12:51:48 +0530

    add home screen layout or activity

--- app/src/main/java/com/example/gatemaster/ui/HomeActivity.java
+++ app/src/main/java/com/example/gatemaster/ui/HomeActivity.java
@@ -31,72 +31,54 @@ public class HomeActivity extends BaseActivity implements  CustomerDetailAdapter
     private RecyclerView recyclerView;
     private CustomerDetailAdapter adapter;
     private ArrayList<CustomerDetailModel> customerDetailModelArrayList;
-    private String userId,terminal;
     private View progressLayout;
     private ProgressWheel progresswheel;
 
-    public static String INVOICENO = "",CUSTOMERACCT="",SIGNATUREID = "", CUSTOMERNAME="";
-    public static String post;
+    View checkInVisitorLayout ;
+    View checkOutVisitorLayout ;
+    View checkInPackageLayout ;
+    View checkOutPackageLayout ;
+
+
+
+
+
+    // Get the Button inside the included layout and set the text
+
 
     @Override
     protected void create(Bundle bundle) {
         inflateView(R.layout.home_portrait);
-        Button btnGetInvoice = findViewById(R.id.btn_get_invoice);
-        executeHomeData();
-        btnGetInvoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                executeInvoiceApi(userId);
-            }
-        });
-        databaseConnection.deleteBlankSign();
+        init();
     }
 
-    private void executeHomeData(){
+    private void init(){
+         checkInVisitorLayout = findViewById(R.id.checkInVisitor);
+         checkOutVisitorLayout = findViewById(R.id.checkOutVisitor);
+        checkInPackageLayout = findViewById(R.id.checkInPackage);
+        checkOutPackageLayout = findViewById(R.id.checkOutPackage);
 
-        if (Util.isNetworkAvailable(this)) {
-            showProgress();
-            userId = sharedPref.getString("userid");
-//            terminal = sharedPref.getString("terminal");
-            executeInvoiceApi(userId);
-        }
-        else{
-            Toast.makeText(this,"Check Your Internet Connection",Toast.LENGTH_SHORT).show();
-        }
 
+        Button checkInButton = checkInVisitorLayout.findViewById(R.id.btn);
+        checkInButton.setText("Check In");
+        Button checkOutButton = checkOutVisitorLayout.findViewById(R.id.btn);
+        checkOutButton.setText("Check Out");
+
+        Button checkinButtonPk = checkInPackageLayout.findViewById(R.id.btn);
+        checkinButtonPk.setText("Check In");
+
+        Button checkOutButtonPk = checkOutPackageLayout.findViewById(R.id.btn);
+        checkOutButtonPk.setText("Check Out");
     }
+
+
     private void initRecycleView(){
         try {
-            recyclerView = (RecyclerView) findViewById(R.id.homeListRv);
+            recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
             RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this);
             recyclerView.setLayoutManager(layoutManager);
-            Cursor cursor = databaseConnection.getInvoiceDetails();
             customerDetailModelArrayList = new ArrayList<>();
-            if (cursor != null){
-                if (cursor.getCount()>0){
-                    cursor.moveToFirst();
-                    String snr = cursor.getString(cursor.getColumnIndexOrThrow("signatureNotRequired"));
 
-                        for (int i=0; i<cursor.getCount(); i++) {
-                            if (cursor.getString(cursor.getColumnIndexOrThrow("signatureNotRequired")).equalsIgnoreCase("false")){
-                            customerDetailModelArrayList.add(new CustomerDetailModel(
-                                    cursor.getString(cursor.getColumnIndexOrThrow("transactionId")),
-                                    cursor.getString(cursor.getColumnIndexOrThrow("customerName")),
-                                    cursor.getString(cursor.getColumnIndexOrThrow("customerAcct"))
-                            ));
-                            cursor.moveToNext();
-                        }
-                    }
-                    if (customerDetailModelArrayList != null){
-                        adapter = new CustomerDetailAdapter(customerDetailModelArrayList);
-                        recyclerView.setAdapter(adapter);
-                        adapter.setClickListener(this);
-                        recyclerView.addItemDecoration(new SpacesItemDecoration(getResources().getDimensionPixelSize(R.dimen.recycler_view_item_width)));
-
-                    }
-                }
-
-            }
         }catch (Exception e){
             e.getMessage();
 
@@ -108,32 +90,11 @@ public class HomeActivity extends BaseActivity implements  CustomerDetailAdapter
     @Override
     public void onItemClick(View view, int position) {
         Log.d("HomeActivity" + "position:::", position + "");
-        TextView custAcct = view.findViewById(R.id.custAcct);
-        TextView invoice = view.findViewById(R.id.invoiceValue);
-        TextView custName = view.findViewById(R.id.customerName);
 
-        String custStr = custAcct.getText().toString();
-        String invoiceVal = invoice.getText().toString();
-        String customerNameSign = custName.getText().toString();
-
-        INVOICENO = invoiceVal;
-        CUSTOMERACCT = custStr;
-        CUSTOMERNAME = customerNameSign;
-        String strId = new SimpleDateFormat("ddmmyyyyhhmmss").format(new Date());
-        SIGNATUREID = strId+custStr;
 
        //Util.pushNext(HomeActivity.this,SignActivity.class);
     }
 
 
-    @Subscribe
-    public void onEvent(InvoiceBusEvent invoiceBusEvent){
-        hideProgress();
-        if (invoiceBusEvent.getStatus()=="YES"){
-            initRecycleView();
-        }
-        else{
-            Toast.makeText(this,"Please try again",Toast.LENGTH_SHORT).show();
-        }
-    }
+
 }
